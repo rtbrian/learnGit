fb35697c4173497e46f3fc77cba2452fc5e8df97
rtbrian <gacherubrian93@gmail.com>
2024-03-11 13:21:44 +0300

    javascript

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ else{
 }*/
 
 
-let a,b ;
+/*let a,b ;
 
 a=4;
 b=6;
@@ -27,4 +27,12 @@ let divide= a/b;
 let add= a+b;
 let subtract= a-b;
 
-console.log("\nadd:",add,"\nsubtract:",subtract,"\ndivide:",divide,"\ntimes:",times);
+console.log("\nadd:",add,"\nsubtract:",subtract,"\ndivide:",divide,"\ntimes:",times);*/
+
+function sum(a,b){
+    let sum=a+b;
+    console.log(sum);
+}
+
+sum(6,4)
+
