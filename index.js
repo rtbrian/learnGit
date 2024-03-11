3118a8c82a43f4a34b6e893e8c64688d381dd060
rtbrian <gacherubrian93@gmail.com>
2024-03-11 12:54:45 +0300

    javascript

---
+++ index.js
@@ -0,0 +1,18 @@
+/*let myname ="brian";
+
+myname = "mwangi";
+
+myname=5;
+
+console.log(myname);
+
+console.log(mynum);*/
+let mynum=6;
+if (mynum===5)
+{
+console.log('True');
+}
+else{
+    console.log('False');
+}
+
