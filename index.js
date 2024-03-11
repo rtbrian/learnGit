1dcb346bb0cbfe1d4b9bdb7135fb7436fab9cade
rtbrian <gacherubrian93@gmail.com>
2024-03-11 13:04:48 +0300

    javascript

--- index.js
+++ index.js
@@ -7,12 +7,24 @@ myname=5;
 console.log(myname);*/
 
 //console.log(mynum);
-let mynum=6;
+/*let mynum=6;
 if (mynum===5)
 {
 console.log('True');
 }
 else{
     console.log('False');
-}
+}*/
 
+
+let a,b ;
+
+a=4;
+b=6;
+
+let times= a*b; 
+let divide= a/b;
+let add= a+b;
+let subtract= a-b;
+
+console.log(add,subtract,divide,times);
